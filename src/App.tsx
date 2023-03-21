470db03f225a37e0882df5c502bfa88660cdb2f8
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-21 20:12:25 +0300

    add: link to about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import MainPage from 'pages/mainPage';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import styles from './App.module.css';
 import AboutPage from 'pages/aboutPage';
 import NotFoundPage from 'pages/notFoundPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"></header> */}
+      <header className={styles.appHeader}>
+        <NavLink className={styles.link} to="/">
+          Home
+        </NavLink>
+        <NavLink className={styles.link} to="/about">
+          About
+        </NavLink>
+      </header>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/about" element={<AboutPage />} />
