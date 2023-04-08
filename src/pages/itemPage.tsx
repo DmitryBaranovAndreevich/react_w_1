b4a5433d92347192bc6c7f16578ab0c280400d48
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-04-08 20:31:21 +0300

    fix: linter errors

--- src/pages/itemPage.tsx
+++ src/pages/itemPage.tsx
@@ -1,7 +1,6 @@
 import BigItem from 'components/bigItem/bigItem';
 import Layout from 'components/layout/layout';
 import Popup from 'components/popup/popup';
-import IGood from 'interfaces/IItem';
 import React from 'react';
 
 const ItemPage = () => {
